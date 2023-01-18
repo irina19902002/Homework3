5f25af8a70395a4ed93f5ddcdfe96daf9465319a
Irina Smirnova <smirnovaiv20.02@gmail.com>
2023-01-18 20:26:18 +0300

    linemetod

--- StringMetod.java
+++ StringMetod.java
@@ -8,24 +8,13 @@ public class StringMetod {
         //Преобразуйте строку к нижнему регистру.
         //Вырезать строку Java c помощью метода String.substring().
         String a = "I like Java!!!";
-        System.out.println(a.startsWith("I like"));
-        strreplace(a);//Заменить все символы “а” на “о”.
-
-
-
-
-    }
-// //Заменить все символы “а” на “о”.
-
-    public static void strreplace(String text) {
-        for (int i = 0; i < text.length(); i++) {
-            if ((text.charAt(i)) == 'a') {
-                System.out.print("o");
-            } else {
-                System.out.print(text.charAt(i));
-            }
-        }
+        System.out.println(a.startsWith("I like"));//Проверить, начинается ли ваша строка подстрокой “I like”. Используем метод String.startsWith().
+        System.out.println(a.indexOf("Java"));//Найти позицию подстроки “Java” в строке “I like Java!!!”.
+        System.out.println(a.replace('a','o'));//Заменить все символы “а” на “о”
+        System.out.println(a.toUpperCase());//verhnij registr
+        System.out.println(a.toLowerCase());//Преобразуйте строку к нижнему регистру.
 
     }
 }
+
 
